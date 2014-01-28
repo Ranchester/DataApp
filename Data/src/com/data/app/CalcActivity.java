3f0887a6d4efd3190cb9f66b0dd5867b05d7ef3b
Ranchester <akreivi@gmail.com>
2014-01-28 14:15:38 +0200

    Data interaction between classes and calculation add

--- Data/src/com/data/app/CalcActivity.java
+++ Data/src/com/data/app/CalcActivity.java
@@ -1,6 +1,7 @@
 package com.data.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -31,7 +32,42 @@ public class CalcActivity extends Activity implements OnClickListener{
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		Intent i = new Intent(this, Numbers.class);
 		
+		switch(v.getId()) {
+		case R.id.button1:
+			// width button
+			i.putExtra("numbers", "width"); //add data to intent,  data = String numbers = "width"
+			startActivityForResult(i, 1);
+			break;
+		case R.id.button2:
+			//height button
+			i.putExtra("numbers", "height"); //add data to intent, data = String numbers = "height"
+			startActivityForResult(i, 1);
+			break;
+		case R.id.button3:
+			//calc button
+			int a;
+			int b;
+			a = Integer.valueOf(width.getText().toString());
+			b = Integer.valueOf(height.getText().toString());
+			area.setText(a*b + "sq feet"); //set square feet to textarea
+			break;
+		
+		}
+							
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		if(data.getExtras().containsKey("widthInfo")){
+			width.setText(data.getStringExtra("widthInfo"));
+		}
+		if(data.getExtras().containsKey("heightInfo")){
+			height.setText(data.getStringExtra("heightInfo"));	
+		}
 	}
 	
 	
--- Data/src/com/data/app/Numbers.java
+++ Data/src/com/data/app/Numbers.java
@@ -2,6 +2,7 @@ package com.data.app;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -26,6 +27,21 @@ public class Numbers extends Activity implements OnClickListener{
     @Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+    	String s = number.getText().toString(); //gets the string to the extras called in CalcActivity putExtra(i, s)
+    	Intent i = getIntent(); //gets intent to the extras called in CalcActivity
+    	String msg = i.getStringExtra("numbers"); //names the extras "numbers" for CalcActivity to call
+    	
+    	if(msg.contentEquals("width")) { //if msg content set to string "width"
+    		i.putExtra("widthInfo", s); //puts "widthInfo" string to extras called in CalcActivity
+    		setResult(RESULT_OK, i);
+    		finish();
+    	}
+    	
+    	if(msg.contentEquals("height")){
+    		i.putExtra("heightInfo", s);
+    		setResult(RESULT_OK, i);
+    		finish();
+    	}
 		
 	}
 }
