5fdfe87f52bc117996395c8bd43a998c57627186
Ranchester <akreivi@gmail.com>
2014-01-29 12:54:38 +0200

    Settings activity add

--- Data/src/com/data/app/Settings.java
+++ Data/src/com/data/app/Settings.java
@@ -1,26 +1,74 @@
 package com.data.app;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
-public class Settings extends Activity {
+public class Settings extends Activity implements OnClickListener {
+	
+	CheckBox cb;
+	EditText et;
+	Button b;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        cb = (CheckBox) findViewById(R.id.checkBox1);
+        et = (EditText) findViewById(R.id.EditText1);
+        b = (Button) findViewById(R.id.button1);
+        b.setOnClickListener(this);
+        loadPrefs(); //loads default preferences
+        
     }
 
+    //loads saved preferences and sets default string for textbox
     private void loadPrefs() {
+    	SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this); //Needed for data handling
+    	boolean cbValue = sp.getBoolean("CHECKBOX", false); //sets default value to "checkbox"
+    	String name = sp.getString("NAME", "Name");
+    	if(cbValue){
+    		cb.setChecked(true);
+    	} else {
+    		cb.setChecked(false);
+    	}
     	
+    	et.setText(name);
     }
     
+    //Saves boolean value for checkbox
     private void savePrefs(String key, boolean value){
-    	
+    	SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this); //Needed for data handling
+    	Editor edit = sp.edit();
+    	edit.putBoolean(key, value);
+    	edit.commit();
     }
     
+    //Saves String for edit text
     private void savePrefs(String key, String value){
-    	    
+    	SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+    	Editor edit = sp.edit();
+    	edit.putString(key, value);
+    	edit.commit();	    
     }
+
+	@Override
+	public void onClick(View v) {
+		savePrefs("CHECKBOX", cb.isChecked());
+		if (cb.isChecked()){
+			savePrefs("NAME", et.getText().toString());
+		}
+		
+		finish(); //closes activity after click
+		
+	}    
+    
 }
---
+++ t
@@ -0,0 +1,29 @@
+[33mcommit 6641ebcdd11754f5374c3bb605eb4b447e52753f[m
+Author: Ranchester <akreivi@gmail.com>
+Date:   Wed Jan 29 11:05:07 2014 +0200
+
+    Launcher menu add
+
+[33mcommit b497ed25c063357d6ebffc32001f09df9dd7a1ff[m
+Author: Ranchester <akreivi@gmail.com>
+Date:   Wed Jan 29 10:58:38 2014 +0200
+
+    Settings activity add
+
+[33mcommit 3f0887a6d4efd3190cb9f66b0dd5867b05d7ef3b[m
+Author: Ranchester <akreivi@gmail.com>
+Date:   Tue Jan 28 14:15:38 2014 +0200
+
+    Data interaction between classes and calculation add
+
+[33mcommit a6cc76e8e15ef4c1e8dbc669be587c1dd72d61c1[m
+Author: Ranchester <akreivi@gmail.com>
+Date:   Tue Jan 28 10:17:11 2014 +0200
+
+    Data App start
+
+[33mcommit 5149a11d703b22d6cedb8ee2ba59c4a9a2636501[m
+Author: Ranchester <akreivi@gmail.com>
+Date:   Fri Jan 17 02:09:58 2014 -0800
+
+    Initial commit
