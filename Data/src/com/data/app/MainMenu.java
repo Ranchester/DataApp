6641ebcdd11754f5374c3bb605eb4b447e52753f
Ranchester <akreivi@gmail.com>
2014-01-29 11:05:07 +0200

    Launcher menu add

---
+++ Data/src/com/data/app/MainMenu.java
@@ -0,0 +1,43 @@
+package com.data.app;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+//List activity class
+public class MainMenu extends ListActivity{
+	
+	String classNames[] = {"CalcActivity", "Settings"};
+	String names[] = {"Data between activities example","Settings" };
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names));
+	}
+	
+	protected void onListItemClick(ListView lv, View v, int position, long id){
+		super.onListItemClick(lv, v, position, id);
+		String openClass = classNames[position];
+		try{
+			Class selected = Class.forName("com.data.app." + openClass);
+			Intent selectedIntent = new Intent(this, selected);
+			startActivity(selectedIntent);
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		//finish();
+	}
+
+}
