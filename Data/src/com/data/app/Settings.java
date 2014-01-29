b497ed25c063357d6ebffc32001f09df9dd7a1ff
Ranchester <akreivi@gmail.com>
2014-01-29 10:58:38 +0200

    Settings activity add

---
+++ Data/src/com/data/app/Settings.java
@@ -0,0 +1,26 @@
+package com.data.app;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Settings extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    private void loadPrefs() {
+    	
+    }
+    
+    private void savePrefs(String key, boolean value){
+    	
+    }
+    
+    private void savePrefs(String key, String value){
+    	    
+    }
+}
